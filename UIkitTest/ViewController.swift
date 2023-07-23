6d60fe0dfa880a7595db30e29d3e011e88b27f72
alexpios <kuchinsky213@gmail.com>
2023-07-23 16:28:25 +0300

    initial

--- UIkitTest/ViewController.swift
+++ UIkitTest/ViewController.swift
@@ -8,12 +8,55 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var segmentedControll: UISegmentedControl!
+    @IBOutlet var mainLabel: UILabel!
+    
+    @IBOutlet var slider: UISlider!
+    
+     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+//MARK: segment Controll
+        segmentedControll.insertSegment(withTitle: "Third", at: 2, animated: false)
+//        MARK: mainlabel
+       mainLabel.text = ""
+        mainLabel.font = mainLabel.font.withSize(30)
+        mainLabel.textAlignment = .center
+        mainLabel.numberOfLines = 2
+        
+        slider.value = 1
+        slider.minimumValue = 1
+        slider.maximumValue = 100
+        slider.minimumTrackTintColor = .green
+        slider.maximumTrackTintColor = .blue
+        slider.thumbTintColor = UIColor(red: 255/255, green: 0/255, blue: 0/255, alpha: 1)
+        mainLabel.text = String(slider.value)
+        
     }
 
-
+    @IBAction func segmentConreollAction(_ sender: Any) {
+        switch segmentedControll.selectedSegmentIndex {
+        case 0:
+            mainLabel.text = "Выбран сегмент 1"
+            mainLabel.textColor = .red
+        case 1:
+    
+            mainLabel.text = "Выбран сегмент 2"
+            mainLabel.textColor = .green
+            
+        case 2:
+            mainLabel.text = "Выбран сегмент 3"
+            mainLabel.textColor = .blue
+        default:
+            break
+        }
+        
+    }
+    
+    @IBAction func sliderAction(_ sender: Any) {
+    }
+    
+    
+    
 }
 
