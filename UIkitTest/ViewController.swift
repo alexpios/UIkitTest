50f249e443852c668539d0f6efeb34e06a904790
alexpios <kuchinsky213@gmail.com>
2023-07-23 12:53:59 +0300

    Initial Commit

---
+++ UIkitTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIkitTest
+//
+//  Created by Alex on 23.07.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
